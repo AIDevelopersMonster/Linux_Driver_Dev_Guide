d5aed347139bde193054877be7b99c76170e2741
AIDevelopersMonster <t89257409519@gmail.com>
2026-09-24 06:01:52 +0300

    Fix Sphinx static path for CI

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,6 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
 # -- Internationalization ----------------------------------------------------
 language = 'ru'
